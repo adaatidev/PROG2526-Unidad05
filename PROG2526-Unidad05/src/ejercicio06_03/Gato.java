d81b28a7db02d785bbf406733d5f31c2d16e1b7a
adaatidev <ada.atienza@iesnervion.es>
2026-04-09 14:19:03 +0200

    Herencia e interfaces

--- PROG2526-Unidad05/src/ejercicio06_03/Gato.java
+++ PROG2526-Unidad05/src/ejercicio06_03/Gato.java
@@ -2,6 +2,8 @@ package ejercicio06_03;
 
 public class Gato extends AnimalDomestico {
 
+	private static final double PROBABILIDAD_OBEDIENCIA = 0.05;
+
 	public Gato(String nombre, String raza, String color, double peso) {
 		super(nombre, raza, color, peso);
 	}
@@ -11,6 +13,9 @@ public class Gato extends AnimalDomestico {
 		System.out.println(getNombre() + ": miau");
 	}
 
-	
-	
+	@Override
+	public boolean hacerCaso() {
+		return Math.random() < PROBABILIDAD_OBEDIENCIA;
+	}
+
 }
